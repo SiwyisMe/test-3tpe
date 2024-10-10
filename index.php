3f0d78fbe3a643a2e3e18b086a0cdba0c7d8630d
Filip Siwinski <dev.siwy@gmail.com>
2024-10-10 12:32:28 +0200

    post requests

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<php
-
 <?php
 $host = "localhost";
 $username = "root";
@@ -10,8 +8,17 @@ $conn = mysqli_connect($host, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-else {
-    echo "<p>Connected successfully</p>";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $car_brand = $_POST['car_brand'];
+    $car_model = $_POST['car_model'];
+    $car_color = $_POST['car_color'];
+    $car_production_date = $_POST['car_production_date'];
+    $car_first_registration_date = $_POST['car_first_registration_date'];
+
+    $sql = "INSERT INTO car (car_brand, car_model, car_color, car_production_date, car_first_registration_date) 
+            VALUES ('$car_brand', '$car_model', '$car_color', '$car_production_date', '$car_first_registration_date')";
 }
+
 
 ?>
